7619416f512301fdf8483d9d308f060d6866d5c9
choikwangseong <cckwang2345@gmail.com>
2021-06-02 23:23:17 +0900

    Revert "no message"
    This reverts commit 888ac341862c919dd75da907745402274af547b7.

--- screen/webERP/src/main/java/com/myspring/Mainplan/MainplanOS/dao/MainPlanOSDAO.java
+++
@@ -1,9 +0,0 @@
-package com.myspring.Mainplan.MainplanOS.dao;
-
-import java.util.List;
-
-public interface MainPlanOSDAO {
-
-	List selectAllMainPlanOSList() throws Exception;
-
-}
--- screen/webERP/src/main/java/com/myspring/Mainplan/dao/MainPlanDAO.java
+++
@@ -1,9 +0,0 @@
-package com.myspring.Mainplan.dao;
-
-import java.util.List;
-
-public interface MainPlanDAO {
-
-	List selectAllMainPlanList() throws Exception;
-
-}
