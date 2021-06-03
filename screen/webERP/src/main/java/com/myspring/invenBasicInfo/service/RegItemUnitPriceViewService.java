a01ee1b06a7b3e40e9cd1d9dfbebc32410d31d73
lunarflos <lunarflos@naver.com>
2021-06-03 16:05:05 +0900

    1차 업로드 주석처리

--- screen/webERP/src/main/java/com/myspring/invenBasicInfo/service/RegItemUnitPriceViewService.java
+++ screen/webERP/src/main/java/com/myspring/invenBasicInfo/service/RegItemUnitPriceViewService.java
@@ -1,11 +1,11 @@
-package com.myspring.invenBasicInfo.service;
-
-import java.util.List;
-
-import org.springframework.dao.DataAccessException;
-
-
-public interface RegItemUnitPriceViewService {
-	public List viewRegItemUnitPrice() throws DataAccessException;
-	public List viewPopRegItemUnitPrice() throws DataAccessException;
-}
+//package com.myspring.invenBasicInfo.service;
+//
+//import java.util.List;
+//
+//import org.springframework.dao.DataAccessException;
+//
+//
+//public interface RegItemUnitPriceViewService {
+//	public List viewRegItemUnitPrice() throws DataAccessException;
+//	public List viewPopRegItemUnitPrice() throws DataAccessException;
+//}
