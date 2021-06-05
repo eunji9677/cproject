454e3966a5685f26b07a4be7e05f09c04fc1add3
DeLancer <rudrms950@gmail.com>
2021-06-05 18:10:08 +0900

    생산파트 수정중

--- screen/webERP/src/main/java/com/myspring/commonProduction/service/CommonProductionService.java
+++ screen/webERP/src/main/java/com/myspring/commonProduction/service/CommonProductionService.java
@@ -1,19 +1,19 @@
-/*
- * package com.myspring.commonProduction.service;
- * 
- * import java.util.List;
- * 
- * import org.springframework.dao.DataAccessException;
- * 
- * import com.myspring.commonProduction.commitOperationInstruction.vo.
- * CommitOperationInstructionVO; import
- * com.myspring.commonProduction.operationRegist.vo.OperationRegistVO;
- * 
- * public interface CommonProductionService {
- * 
- * public List<CommitOperationInstructionVO> selectAllProductionPlanInfo()
- * throws DataAccessException; public List<OperationRegistVO>
- * selectAllOperationInfo() throws DataAccessException;
- * 
- * }
- */
+
+ package com.myspring.commonProduction.service;
+  
+  import java.util.List;
+  
+  import org.springframework.dao.DataAccessException;
+  
+  import com.myspring.commonProduction.commitOperationInstruction.vo.
+  CommitOperationInstructionVO; import
+  com.myspring.commonProduction.operationRegist.vo.OperationRegistVO;
+  
+  public interface CommonProductionService {
+  
+  public List<CommitOperationInstructionVO> selectAllProductionPlanInfo()
+  throws DataAccessException; public List<OperationRegistVO>
+  selectAllOperationInfo() throws DataAccessException;
+  
+  }
+ 
